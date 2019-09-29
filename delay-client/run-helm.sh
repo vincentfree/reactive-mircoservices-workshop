dba64def118923720f63a356b8fcd3316892782a
Vincent Free <vincent.free@ing.com>
2019-09-29 11:01:59 +0200

    forgot renameing the service in this file..

--- delay-client/run-helm.sh
+++ delay-client/run-helm.sh
@@ -1,6 +1,6 @@
 if [[ -n $1 ]]; then
-  helm template --name "$1" --output-dir target vertx-client-service
+  helm template --name "$1" --output-dir target vertx-delay-service
   else
-    helm template --name test-v1 --output-dir target vertx-client-service
+    helm template --name test-v1 --output-dir target vertx-delay-service
 fi
 
