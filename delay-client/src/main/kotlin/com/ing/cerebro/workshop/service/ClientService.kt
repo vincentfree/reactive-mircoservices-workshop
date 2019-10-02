69ae5446228c9436969aefe338c60cb097603a16
Vincent Free <vincent.free@ing.com>
2019-10-02 20:44:58 +0200

    fix problem

--- delay-client/src/main/kotlin/com/ing/cerebro/workshop/service/ClientService.kt
+++ delay-client/src/main/kotlin/com/ing/cerebro/workshop/service/ClientService.kt
@@ -4,10 +4,13 @@ import com.ing.cerebro.workshop.core.ContentTypes
 import com.ing.cerebro.workshop.core.RouterService
 import io.vertx.core.Vertx
 import io.vertx.core.http.HttpHeaders
+import io.vertx.core.impl.logging.Logger
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 
 class ClientService(private val router: Router, private val vertx: Vertx) : RouterService {
+    override val logger: Logger = logger()
+
     override fun finalize(): Router = router.apply {
         get("/client/:time").produces(ContentTypes.plainText).handler(clientResult)
     }
